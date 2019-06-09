efc4cfc1ae359c7992247525609d98649b3f2dd4
mrstexx <stefi5miljevic@gmail.com>
2019-06-09 22:05:25 +0200

    Image manager design part 1

--- inc/manager.php
+++ inc/manager.php
@@ -2,7 +2,45 @@
 /**
  * Panel to work with images (geo, adding, cutting, delete, duplicate, ...)
  */
-
 ?>
+<div class="d-flex" id="wrapper">
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+        <div class="sidebar-heading">Image Controls</div>
+        <div class="list-group list-group-flush">
+            <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-plus-circle"></i> Add
+                new image</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-edit"></i> Edit images</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-map-marked-alt"></i>
+                Geo Position</a>
+        </div>
+    </div>
 
-<h2>Manager</h2>
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <h1 class="mt-4">
+                <button class="btn btn-secondary" id="menu-toggle"><i class="fas fa-exchange-alt"></i></button>
+                Image Control
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
+                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+                clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            </p>
+        </div>
+    </div>
+</div>
+
+<script src="js/ManagerHelper.js"></script>
