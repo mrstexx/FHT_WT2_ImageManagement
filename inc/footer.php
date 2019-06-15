908e9fb2769e97c1985baf4ac350de501268c6cf
UnrealDisaster <marius.hochwald@gmail.com>
2019-06-15 16:12:19 +0200

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <div class="footer-copyright text-center py-3">© 2019 Fachhochschule Technikum Wien
+    <div class="footer-copyright text-center py-3">© 2019 MilHochGrub GmbH
 		<a href="?page=impressum">Impressum</a>
     </div>
 </footer>
