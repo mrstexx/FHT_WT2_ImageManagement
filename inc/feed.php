fe15a75c87c2f703503d1ce4654488cb71afd291
UnrealDisaster <marius.hochwald@gmail.com>
2019-06-16 22:38:01 +0200

    adjusted feed

--- inc/feed.php
+++ inc/feed.php
@@ -1,40 +1,41 @@
 <?php
 include "model/Image.php";
 ?>
+<div class="container col-md-12">
+<h3 class="mt-5 mb-5 ml-5">List of your images</h5>
 
-<h2 class="registration-area">Feed</h2>
-
-<h5>List of your images</h5>
 <div id="myImages">
     <?php
-    echo "<div class=\"row\">";
+	echo "<div class=\"row\">";
     $images = Image::getAllUserImages($_SESSION["user"]);
     for ($i = 0; $i < sizeof($images); $i++) {
-        echo "<div class=\"col-md-6 col-lg-3 my-image registration-area\">";
-        echo "<img id=\"" . $images[$i]["pk_bild_id"] . "\" src=\"" . $images[$i]["thumbnail_directory"] . "\" class=\"img-fluid card-img-top\" alt=\"" . $images[$i]["name"] . "\">";
         $imageplace = $images[$i]["name"];
 		$newimage = explode('.', $imageplace, -1);
-		echo "<h6 class='text-center'> Name: " . $newimage[0] . "</h6>";
+		echo "<div class=\"col-md-6 col-lg-3 my-image ml-5 mr-5 mt-2 registration-area\">";
+		echo "<h6 class='text-center'> Name: " . $newimage[0] . "</h6><a href=\"#\" class=\"btn btn-primary mb-3\">Show on Map</a>";
+        echo "<img id=\"" . $images[$i]["pk_bild_id"] . "\" src=\"" . $images[$i]["thumbnail_directory"] . "\" class=\"img-fluid\" alt=\"" . $images[$i]["name"] . "\">";
 		echo "<p class='text-center'> Das Bild wurde geschossen am " . $images[$i]["aufnahmedatum"] . "</p>";
-        echo "</div>";
+		echo "</div>";
     }
     echo "</div>"
     ?>
-	<h5>List of others images</h5>
+	<h3 class="mt-5 mb-5 ml-5">List of others images</h5>
 	<?php
-    echo "<div class=\"row\">";
+	echo "<div class=\"row\">";
     $images = Image::getAllImages($_SESSION["user"]);
     for ($i = 0; $i < sizeof($images); $i++) {
-        echo "<div class=\"col-md-6 col-lg-3 my-image\">";
-        echo "<img id=\"" . $images[$i]["pk_bild_id"] . "\" src=\"" . $images[$i]["thumbnail_directory"] . "\" class=\"img-fluid\" alt=\"" . $images[$i]["name"] . "\">";
         $imageplace = $images[$i]["name"];
 		$newimage = explode('.', $imageplace, -1);
-		echo "<h6 class='text-center'>" . $newimage[0] . "</h6>";
+		echo "<div class=\"col-md-6 col-lg-3 my-image ml-4 mr-4 mt-2 mb-3 registration-area\">";
+        echo "<h6 class='text-center'> Name: " . $newimage[0] . "</h6><a href=\"#\" class=\"btn btn-primary mb-3\">Show on Map</a>";
+		echo "<img id=\"" . $images[$i]["pk_bild_id"] . "\" src=\"" . $images[$i]["thumbnail_directory"] . "\" class=\"img-fluid\" alt=\"" . $images[$i]["name"] . "\">";
+		echo "<p class='text-center'> Das Bild wurde geschossen am " . $images[$i]["aufnahmedatum"] . "</p>";
         echo "<div class=\"img-options\">";
         echo "</div>";
-        echo "</div>";
-    }
-    echo "</div>"
-    ?>
 
-<h2>Feed</h2>
+    }
+	echo "</div>";
+    echo "</div>";
+    ?>
+</div>
+</div>
